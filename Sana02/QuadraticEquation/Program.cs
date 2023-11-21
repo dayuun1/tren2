84784dffdc92d6620c7867f13739814b79d345df
Dan Vonsovic <ipz232_vdb@student.ztu.edu.ua>
2023-11-21 13:23:39 +0200

    PointsAverage

--- Sana02/QuadraticEquation/Program.cs
+++ Sana02/QuadraticEquation/Program.cs
@@ -1,34 +1,37 @@
-﻿
-    internal class Program
+﻿namespace QuadraticEquation
 {
-    static void Main(string[] args)
+    internal class Program
     {
-        Console.WriteLine("Enter a: ");
-        double a = double.Parse(Console.ReadLine());
-
-        Console.WriteLine("Enter b: ");
-        double b = double.Parse(Console.ReadLine());
-
-        Console.WriteLine("Enter c: ");
-        double c = double.Parse(Console.ReadLine());
-
-        double discriminant = b * b - 4 * a * c;
-
-        if (discriminant > 0)
+        static void Main(string[] args)
         {
-            double x1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
-            double x2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
-            Console.WriteLine($"Two solutions: x1 = {x1}, x2 = {x2}");
+            Console.WriteLine("Enter a: ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter b: ");
+            double b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter c: ");
+            double c = double.Parse(Console.ReadLine());
+
+            double discriminant = b * b - 4 * a * c;
+
+            if (discriminant > 0)
+            {
+                double x1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+                Console.WriteLine($"Two solutions: x1 = {x1}, x2 = {x2}");
+            }
+            else if (discriminant == 0)
+            {
+                double x = -b / (2 * a);
+                Console.WriteLine($"One solution: x = {x}");
+            }
+            else
+            {
+                Console.WriteLine("There are no solutions");
+            }
+            Console.ReadKey();
         }
-        else if (discriminant == 0)
-        {
-            double x = -b / (2 * a);
-            Console.WriteLine($"One solution: x = {x}");
-        }
-        else
-        {
-            Console.WriteLine("There are no solutions");
-        }
-        Console.ReadKey();
     }
 }
+
